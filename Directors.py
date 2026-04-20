effa11fd3140c84cb2de58284c2c78b596923f0b
Zeppyy <csmmscri@ljmu.ac.uk>
2026-04-20 11:52:50 +0100

    Complete full graphs
    Added 2 graphs to complete the requirements.

--- Directors.py
+++ Directors.py
@@ -5,7 +5,7 @@ df = pd.read_csv('imdb_top_1000.csv')
 
 df['Gross'] = df['Gross'].str.replace(',', '').astype(float)
 
-fig, axes = plt.subplots(2,2, figsize=(14, 10))
+fig, axes = plt.subplots(2, 2, figsize=(14, 10))
 fig.suptitle('IMDb Director Analysis')
 
 # Question 1 - Which director appears the most in the top 1000?
@@ -31,6 +31,28 @@ axes[0, 1].set_xlabel('Director')
 axes[0, 1].set_ylabel('Average rating')
 axes[0, 1].set_ylim(8, top_ratings.values.max() + 0.1)
 axes[0, 1].tick_params(axis='x', rotation=45)
+
+# Question 3 - Which director has highest average revenue in the top 1000?
+avg_revenue = df.groupby('Director')['Gross'].mean()
+top_revenue = avg_revenue.sort_values(ascending=False).head(10)
+
+axes[1, 0].bar(top_revenue.index, top_revenue / 1000000)
+axes[1, 0].set_title('Highest average revenue')
+axes[1, 0].set_xlabel('Director')
+axes[1, 0].set_ylabel('Average gross (millions $)')
+axes[1, 0].tick_params(axis='x', rotation=45)
+
+# Question 4 - Which directors get higher ratings in the top 1000?
+top5_directors = director_counts.head(5).index
+
+for director in top5_directors:
+    ratings = df[df['Director'] == director]['IMDB_Rating']
+    axes[1, 1].scatter([director] * len(ratings), ratings)
+
+axes[1, 1].set_title('Ratings per director')
+axes[1, 1].set_xlabel('Director')
+axes[1, 1].set_ylabel('IMDb Rating')
+axes[1, 1].tick_params(axis='x', rotation=45)
 
 plt.tight_layout()
 plt.show()
